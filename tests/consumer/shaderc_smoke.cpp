8170a5ac1580f926fde87d62267107af9d9076d3
Thomas Edvalson <machin3@gmail.com>
2026-09-15 19:15:47 -0500

    Fix bgfx public headers in release package

--- tests/consumer/shaderc_smoke.cpp
+++ tests/consumer/shaderc_smoke.cpp
@@ -1,3 +1,6 @@
+#include <bgfx/src/vertexlayout.h>
+#include <bgfx/tools/shaderc/shaderc.h>
+
 extern int noveltea_bgfx_shaderc_embedded_cli_main(int argc, const char* argv[]);
 
 int main(int argc, const char* argv[]) {
